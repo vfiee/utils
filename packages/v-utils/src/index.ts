38990c14c35fe9307359192a8e480320b1e832e6
vyron <fiee_vyron@163.com>
2022-04-02 14:53:44 +0800

    feat: 新增v-utils 包

---
+++ packages/v-utils/src/index.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: vyron
+ * @Date: 2022-04-02 14:12:59
+ * @LastEditTime: 2022-04-02 14:15:23
+ * @LastEditors: vyron
+ * @Description: 融合汇聚工具类导出
+ * @FilePath: /v-utils/packages/v-utils/src/index.ts
+ */
+export * from '@v-utils/array'
+
+export * from '@v-utils/type'
