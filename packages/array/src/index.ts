768660c6e6aaaa9dd3461d008c7bd3c1923c93d8
vyron <fiee_vyron@163.com>
2022-04-11 16:16:09 +0800

    refactor(array): 调整顺序

--- packages/array/src/index.ts
+++ packages/array/src/index.ts
@@ -1,13 +1,27 @@
 /*
  * @Author: vyron
  * @Date: 2022-03-24 14:48:36
- * @LastEditTime: 2022-04-02 14:35:46
+ * @LastEditTime: 2022-04-11 15:47:50
  * @LastEditors: vyron
  * @Description: 数组常用方法
  * @FilePath: /v-utils/packages/array/src/index.ts
  */
 
 import { isArray } from '@v-utils/type'
+
+// 返回数组的第一个元素,如果没有则返回 undefined
+export const head = (array: any[]) => (isArray(array) ? array[0] : undefined)
+
+export const first = head
+
+// 返回数组的最后一个元素,如果没有则返回 undefined
+export const last = (array: any[]) =>
+  isArray(array) ? array[array.length - 1] : undefined
+
+export const tail = last
+
+// 过滤并返回数组中所有的 truthy 值元素
+export const compact = (array: any[]) => array.filter(Boolean)
 
 // 将数组拆分成多个长度为 size 的块,并将多个块组成一个新数组并返回;
 // 如果 array 无法被分割成全部等长的块,剩余的元素将组成一个块
@@ -20,18 +34,6 @@ export const chunk = (array: any[], size: number = 1) => {
   }
   return result
 }
-
-// 过滤并返回数组中所有的 truthy 值元素
-export const compact = (array: any[]) => array.filter(Boolean)
-
-// 返回数组的第一个元素,如果没有则返回 undefined
-export const head = (array: any[]) => (isArray(array) ? array[0] : undefined)
-
-export const first = head
-
-// 返回数组的最后一个元素,如果没有则返回 undefined
-export const last = (array: any[]) =>
-  isArray(array) ? array[array.length - 1] : undefined
 
 // 根据 depth 递归减少 array 的嵌套层级
 export const flattenDepth = (array: any[], depth = 1): any[] => {
