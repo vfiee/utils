cdb8526cda71cbbb81230d57789640f9ebc12521
vyron <fiee_vyron@163.com>
2022-07-10 23:01:42 +0800

    feat(storage): 新增 storage 包

--- packages/storage/__tests__/index.spec.ts
+++ packages/storage/__tests__/index.spec.ts
@@ -1,48 +1,234 @@
-import { LocalStorage } from '../src'
+import { LocalStorage } from "../src"
 
-describe('localStorage', () => {
-  const localStorage = new LocalStorage()
+jest.useFakeTimers()
+describe("localStorage", () => {
+	const storage = new LocalStorage()
+	const prefixStorage = new LocalStorage({ prefix: "__test__" })
+	const cryptoStorage = new LocalStorage({ enableCrypto: true })
 
-  // const cryptoLocalStorage = new LocalStorage({ enableSecret: true })
+	const key = "test"
+	const value = { data: "I'm test data" }
 
-  it('should return null if localStorage has not the key', () => {
-    expect(localStorage.get('demo')).toBeNull()
-  })
-  it('should return true if set localStorage success', () => {
-    const key = 'testKey'
-    const value = { isTest: true }
-    expect(localStorage.set(key, value)).toBe(true)
-    expect(localStorage.length).toBe(1)
-    expect(localStorage.key(0)).toBe(key)
-  })
-  it('should get localStorage', () => {
-    expect(localStorage.get('demo')).toBeNull()
-    expect(localStorage.get('testKey')).toEqual({ isTest: true })
-  })
-  it('should remove localStorage', () => {
-    localStorage.remove('testKey')
-    expect(localStorage.key(0)).toBeNull()
-    expect(localStorage.length).toBe(0)
-    expect(localStorage.get('testKey')).toBeNull()
-  })
-  it('should clear all localStorage', () => {
-    const length = 10
-    const list = Array.from(Array(length)).map((_, index) => ({
-      key: index.toString(),
-      value: { value: index }
-    }))
-    for (let i = 0; i < list.length; i++) {
-      const { key, value } = list[i]
-      localStorage.set(key, value)
-    }
-    expect(localStorage.length).toBe(length)
-    expect(localStorage.get(list[0].key)).toEqual(list[0].value)
-    expect(localStorage.get(list[length - 1].key)).toEqual(
-      list[length - 1].value
-    )
-    localStorage.clear()
-    expect(localStorage.length).toBe(0)
-    expect(localStorage.get(list[0].key)).toBeNull()
-    expect(localStorage.get(list[length - 1].key)).toBeNull()
-  })
+	it("get: should return null if storage has not the key", () => {
+		expect(storage.set(key, value)).toBe(true)
+		expect(storage.get("demo")).toBeNull()
+		expect(storage.length).toBe(1)
+		storage.clear()
+	})
+
+	it("get: should return null if storage is empty", () => {
+		expect(storage.length).toBe(0)
+		expect(storage.get(key)).toBeNull()
+	})
+
+	it("get: should return data if storage has the key", () => {
+		expect(storage.set(key, value)).toBe(true)
+		expect(storage.length).toBe(1)
+		expect(storage.get(key)).toEqual(value)
+		storage.clear()
+	})
+
+	it("get: should return data if set storage by storage origin method", () => {
+		const data = JSON.stringify(value)
+		expect(localStorage.setItem(key, data)).toBeUndefined()
+		expect(localStorage.length).toBe(1)
+		expect(storage.length).toBe(1)
+		expect(localStorage.getItem(key)).toEqual(data)
+		expect(storage.get(key)).toEqual(data)
+		storage.clear()
+	})
+
+	it("get: should decrypt if storage encrypted", () => {
+		expect(cryptoStorage.set(key, value)).toBe(true)
+		expect(cryptoStorage.length).toBe(1)
+		expect(cryptoStorage.get(key)).toEqual(value)
+		storage.clear()
+	})
+
+	it("set: should return true if set storage success", () => {
+		expect(storage.set(key, value)).toBe(true)
+		expect(storage.length).toBe(1)
+		expect(storage.key(0)).toBe(key)
+		storage.clear()
+	})
+
+	it("get: should return null if storage is expired", () => {
+		// expect storage will expired after 10 seconds
+		expect(storage.set(key, value, 10)).toBe(true)
+		expect(storage.length).toBe(1)
+		expect(storage.get(key)).toEqual(value)
+		setTimeout(() => {
+			expect(storage.get(key)).toBe(null)
+			expect(storage.length).toBe(0)
+			storage.clear()
+		}, 11000)
+		jest.runAllTimers()
+	})
+
+	it("remove: should return void if storage is empty", () => {
+		expect(storage.remove(key)).toBeUndefined()
+		expect(storage.length).toBe(0)
+	})
+
+	it("remove: should return void if remove key is empty", () => {
+		expect(storage.remove("")).toBeUndefined()
+		expect(storage.length).toBe(0)
+	})
+
+	it("remove: should remove the key of the storage", () => {
+		expect(storage.set(key, value)).toBe(true)
+		expect(storage.length).toBe(1)
+		expect(storage.get(key)).toEqual(value)
+		expect(storage.remove(key)).toBeUndefined()
+		expect(storage.length).toBe(0)
+		expect(storage.get(key)).toBeNull()
+	})
+
+	it("remove: should remove the keys of the storage", () => {
+		const key2 = "key2"
+		const value2 = { value: "value2" }
+		expect(storage.set(key, value)).toBe(true)
+		expect(storage.set(key2, value2)).toBe(true)
+		expect(storage.length).toBe(2)
+		expect(storage.get(key)).toEqual(value)
+		expect(storage.get(key2)).toEqual(value2)
+		expect(storage.remove([key, key2])).toBeUndefined()
+		expect(storage.length).toBe(0)
+		expect(storage.get(key)).toBeNull()
+		expect(storage.get(key2)).toBeNull()
+	})
+
+	it("clear: should clear all storage", () => {
+		const length = 10
+		const list = Array.from(Array(length)).map((_, index) => ({
+			key: index.toString(),
+			value: { value: index }
+		}))
+		const first = list[0]
+		const last = list[length - 1]
+		for (let i = 0; i < list.length; i++) {
+			const { key, value } = list[i]
+			storage.set(key, value)
+		}
+		expect(storage.length).toBe(length)
+		expect(storage.get(first.key)).toEqual(first.value)
+		expect(storage.get(last.key)).toEqual(last.value)
+		storage.clear()
+		expect(storage.length).toBe(0)
+		expect(storage.get(first.key)).toBeNull()
+		expect(storage.get(last.key)).toBeNull()
+	})
+
+	it("clear: should clear all storage except excludes", () => {
+		const length = 10
+		const list = Array.from(Array(length)).map((_, index) => ({
+			key: index.toString(),
+			value: { value: index }
+		}))
+		const first = list[0]
+		const last = list[length - 1]
+		for (let i = 0; i < list.length; i++) {
+			const { key, value } = list[i]
+			storage.set(key, value)
+		}
+		expect(storage.length).toBe(length)
+		expect(storage.get(first.key)).toEqual(first.value)
+		expect(storage.get(last.key)).toEqual(last.value)
+		storage.clear([first.key, last.key])
+		expect(storage.length).toBe(2)
+		expect(storage.get(first.key)).toEqual({ value: 0 })
+		expect(storage.get(last.key)).toEqual({ value: 9 })
+		storage.clear()
+	})
+
+	it("length: should return 0 if storage is empty", () => {
+		expect(storage.length).toBe(0)
+		expect(storage.isEmpty).toBe(true)
+	})
+
+	it("length: should return storage length if storage is't empty", () => {
+		storage.set(key, value)
+		expect(storage.length).toBe(1)
+		storage.clear()
+	})
+
+	it("key: should return null if storage has't the key", () => {
+		expect(storage.key(100)).toBeNull()
+	})
+
+	it("key: should return the key if storage has the key", () => {
+		storage.set(key, value)
+		expect(storage.key(0)).toBe(key)
+		storage.clear()
+	})
+
+	it("key: should return unPrefix KEY if storage config the prefix", () => {
+		prefixStorage.set(key, value)
+		expect(prefixStorage.key(0)).toBe(key)
+		expect(prefixStorage.get(key)).toEqual(value)
+		storage.clear()
+	})
+
+	it("keys: should return empty array if storage is empty", () => {
+		expect(storage.keys()).toEqual([])
+	})
+
+	it("keys: should return key list if storage is not empty", () => {
+		storage.set(key, value)
+		storage.set("key2", { value: "key2" })
+		expect(storage.keys()).toEqual([key, "key2"])
+		storage.clear()
+	})
+
+	it("keys: should return unPrefix key list if storage config the prefix", () => {
+		prefixStorage.set(key, value)
+		prefixStorage.set("key2", { value: "key2" })
+		expect(prefixStorage.keys()).toEqual([key, "key2"])
+		storage.clear()
+	})
+
+	it("values: should return empty list if storage is empty", () => {
+		expect(storage.length).toBe(0)
+		expect(storage.values()).toEqual([])
+	})
+
+	it("values: should return values if storage is not empty", () => {
+		const length = 10
+		const list = Array.from(Array(length)).map((_, index) => ({
+			key: index.toString(),
+			value: { value: index }
+		}))
+		for (let i = 0; i < list.length; i++) {
+			const { key, value } = list[i]
+			storage.set(key, value)
+		}
+		const values = storage.values()
+		expect(values.length).toBe(length)
+		expect(values).toEqual(list.map(item => item.value))
+		storage.clear()
+	})
+
+	it("values: should be null if storage item was expired", () => {
+		const length = 5
+		const list = Array.from(Array(length)).map((_, index) => ({
+			key: index.toString(),
+			value: { value: index }
+		}))
+		for (let i = 0; i < list.length; i++) {
+			const { key, value } = list[i]
+			storage.set(key, value)
+		}
+		// storage will expired after 5 seconds
+		storage.set(key, value, 5)
+		const values = storage.values()
+		expect(values.length).toBe(6)
+		expect(values).toEqual([...list.map(item => item.value), value])
+		setTimeout(() => {
+			const values = storage.values()
+			expect(values.length).toBe(6)
+			expect(values).toEqual([...list.map(item => item.value), null])
+			storage.clear()
+		}, 6000)
+		jest.runAllTimers()
+	})
 })
--- packages/storage/src/index.ts
+++ packages/storage/src/index.ts
@@ -1,229 +1,213 @@
 /*
  * @Author: vyron
  * @Date: 2022-05-19 17:57:31
- * @LastEditTime: 2022-06-27 23:59:54
+ * @LastEditTime: 2022-07-11 18:39:56
  * @LastEditors: vyron
  * @Description: 融合汇聚工具类导出
  * @FilePath: /utils/packages/storage/src/index.ts
  */
 
-import { isString, isNil, isNumber, isDate } from '@vyron/utils'
-import AES from 'crypto-js/aes'
-import encBase64 from 'crypto-js/enc-base64'
-import encHex from 'crypto-js/enc-hex'
-import encUtf8 from 'crypto-js/enc-utf8'
-import modCfb from 'crypto-js/mode-cfb'
-import padPkcs7 from 'crypto-js/pad-pkcs7'
+import { isString, isNil, isNumber, isDate } from "@vyron/utils"
+import AES from "crypto-js/aes"
+import encBase64 from "crypto-js/enc-base64"
+import encHex from "crypto-js/enc-hex"
+import encUtf8 from "crypto-js/enc-utf8"
+import modCfb from "crypto-js/mode-cfb"
+import padPkcs7 from "crypto-js/pad-pkcs7"
 
-export interface StorageSecretConfig {
-  encrypt: (v: string) => string
-  decrypt: <T>(v: string) => T
+export interface StorageCryptoConfig {
+	encrypt: (v: string) => string
+	decrypt: <T>(v: string) => T
 }
 
 export interface StorageConfig {
-  enableSecret?: boolean
-  secret?: StorageSecretConfig
-  expire?: string | number | Date
-  prefix?: string
+	enableCrypto?: boolean
+	crypto?: StorageCryptoConfig
+	expire?: string | number | Date
+	prefix?: string
 }
 
 export interface StorageData<T = any> {
-  expire: number
-  origin: T
+	expire?: number
+	origin: T
 }
+
+const $encryptKey = encUtf8.parse("storage_encrypt_key")
+const $encryptIv = encUtf8.parse("storage_encrypt_iv")
 
 const defaultStorageConfig: StorageConfig = {
-  enableSecret: false,
-  secret: {
-    encrypt,
-    decrypt
-  }
+	enableCrypto: false,
+	crypto: {
+		encrypt: function encrypt(value: string): string {
+			if (!value) return value
+			const data = AES.encrypt(encUtf8.parse(value), $encryptKey, {
+				iv: $encryptIv,
+				mode: modCfb,
+				padding: padPkcs7
+			})
+			return data.ciphertext.toString()
+		},
+		decrypt: function decrypt(value: string): any {
+			return AES.decrypt(
+				encBase64.stringify(encHex.parse(value)),
+				$encryptKey,
+				{
+					iv: $encryptIv,
+					mode: modCfb,
+					padding: padPkcs7
+				}
+			).toString(encUtf8)
+		}
+	}
 }
 
-const encryptKey = encUtf8.parse('vyron_storage_encrypt_key')
-const encryptIv = encUtf8.parse('vyron_storage_encrypt_iv')
-
-function encrypt(value: string): string {
-  if (!value) return value
-  const data = AES.encrypt(encUtf8.parse(value), encryptKey, {
-    iv: encryptIv,
-    mode: modCfb,
-    padding: padPkcs7
-  })
-  return data.ciphertext.toString()
-}
-
-function decrypt(value: string): any {
-  return AES.decrypt(encBase64.stringify(encHex.parse(value)), encryptKey, {
-    iv: encryptIv,
-    mode: modCfb,
-    padding: padPkcs7
-  }).toString(encUtf8)
-}
-
-function getDataSize(data: string): number {
-  if (!(data && data.length)) return 0
-  let total = 0
-  let index = -1
-  const length = data.length
-  if (length <= 0) return total
-  while (++index < length) {
-    total += data.charCodeAt(index) <= 0xffff ? 2 : 4
-  }
-  return total
-}
-
-function getTimestampByExpire(expire?: StorageConfig['expire']): number {
-  if (isNil(expire)) return 0
-  if (isString(expire)) {
-    try {
-      return new Date(expire).getTime()
-    } catch (error) {
-      warn(error)
-      return 0
-    }
-  }
-  if (isDate(expire)) {
-    return expire.getTime()
-  }
-  if (isNumber(expire)) {
-    const now = +new Date()
-    return now + expire * 1000
-  }
-  return 0
+function $getTimestampByExpire(expire?: StorageConfig["expire"]): number {
+	if (isNil(expire)) return 0
+	if (isString(expire)) {
+		try {
+			return new Date(expire).getTime()
+		} catch (error) {
+			warn(error)
+			return 0
+		}
+	}
+	if (isDate(expire)) {
+		return expire.getTime()
+	}
+	if (isNumber(expire)) {
+		const now = +new Date()
+		return now + expire * 1000
+	}
+	return 0
 }
 
 function warn(message: any): void {
-  console.warn('[Storage Warn]:', message)
+	console.warn("[Storage Warn]:", message)
 }
 
 class StorageCore {
-  #config: StorageConfig
-  #storage: Storage
-  constructor(storage: Storage, config?: StorageConfig) {
-    this.#storage = storage
-    this.#config = Object.assign({}, defaultStorageConfig, config)
-  }
-  get(key: string): any | null {
-    if (!key) return null
-    try {
-      // 判断storage是否存在
-      const string = this.#storage.getItem(this.#prefixKey(key))
-      if (string === null) return string
-      // 如果加密,则解密
-      const decrypt = this.#config.enableSecret && this.#config.secret?.decrypt
-      const { origin, expire } = JSON.parse(
-        decrypt ? decrypt(string) : string
-      ) as StorageData
-      if (expire) {
-        const now = +new Date()
-        if (now >= expire) {
-          this.remove(key)
-          return null
-        }
-      }
-      return origin
-    } catch (error) {
-      warn(error)
-    }
-    return null
-  }
-  set(key: string, value: any, expire?: StorageConfig['expire']): boolean {
-    let success = false
-    try {
-      let data = JSON.stringify({
-        origin: value,
-        expire: getTimestampByExpire(expire)
-      })
-      const { enableSecret, secret } = this.#config
-      if (enableSecret && secret) {
-        data = secret.encrypt(data)
-      }
-      this.#storage.setItem(this.#prefixKey(key), data)
-      success = true
-    } catch (error) {
-      warn(error)
-      success = false
-    }
-    return success
-  }
-  remove(key: string | string[]): void {
-    if (isString(key)) {
-      return this.#storage.removeItem(this.#prefixKey(key))
-    }
-    let index = -1
-    const length = key.length
-    while (++index < length) {
-      this.#storage.removeItem(this.#prefixKey(key[index]))
-    }
-  }
-  clear(excludes?: string | string[]): void {
-    if (this.length <= 0) return
-    if (!excludes || excludes.length <= 0) {
-      this.#storage.clear()
-      return
-    }
-    const keys: string[] = isString(excludes) ? [excludes] : excludes
-    this.remove(this.keys().filter(key => !keys.includes(key)))
-  }
-  get length(): number {
-    return this.#storage.length
-  }
-  get size(): number {
-    const values = this.values()
-    return getDataSize(values.toString())
-  }
-  key(index: number): string | null {
-    if (!this.length) return null
-    const key = this.#storage.key(index)
-    if (key === null || !this.#config.prefix) return key
-    return key.slice(this.#config.prefix.length)
-  }
-  keys(): string[] {
-    return Object.keys(this.#storage)
-  }
-  values(): string[] {
-    if (this.length <= 0) return []
-    const values = Object.values(this.#storage)
-    if (!this.#config.enableSecret) return this.#transformValue(values)
-    const { decrypt } = this.#config.secret || {}
-    return this.#transformValue(values, decrypt)
-  }
-  #transformValue(
-    values: string[],
-    decrypt?: StorageSecretConfig['decrypt']
-  ): any[] {
-    return values
-      .map((value, index) => {
-        try {
-          const { origin, expire } =
-            JSON.parse(decrypt ? decrypt(value) : value) || {}
-          if (Date.now() >= expire) {
-            const key = this.key(index) as string
-            this.remove(key.slice(this.#config.prefix?.length ?? 0))
-            return null
-          }
-          return origin
-        } catch (error) {
-          warn(error)
-          return null
-        }
-      })
-      .filter(v => !isNil(v))
-  }
-  #prefixKey(key: string): string {
-    return `${this.#config.prefix || ''}${key}`
-  }
+	#config: StorageConfig
+	#storage: Storage
+	constructor(storage: Storage, config?: StorageConfig) {
+		this.#storage = storage
+		this.#config = Object.assign({}, defaultStorageConfig, config)
+	}
+	get(key: string): any | null {
+		if (!key || this.length <= 0) return null
+		// 判断storage是否存在
+		const data = this.#storage.getItem(this.#prefixKey(key))
+		if (data === null) return data
+		try {
+			// 如果加密,则解密
+			const decrypt = this.#config.enableCrypto && this.#config.crypto?.decrypt
+			const { origin, expire } = (JSON.parse(decrypt ? decrypt(data) : data) ||
+				{}) as StorageData
+			if (expire) {
+				const now = +new Date()
+				if (now >= expire) {
+					this.remove(key)
+					return null
+				}
+			}
+			if (origin) {
+				return origin
+			}
+		} catch (error) {
+			warn(error)
+		}
+		return data
+	}
+	set(key: string, value: any, expire?: StorageConfig["expire"]): boolean {
+		let success = false
+		try {
+			let storageData: StorageData = {
+				origin: value
+			}
+			if (expire) {
+				storageData["expire"] = $getTimestampByExpire(expire)
+			}
+			let data = JSON.stringify(storageData)
+			const { enableCrypto, crypto } = this.#config
+			if (enableCrypto && crypto) {
+				data = crypto.encrypt(data)
+			}
+			this.#storage.setItem(this.#prefixKey(key), data)
+			success = true
+		} catch (error) {
+			warn(error)
+			success = false
+		}
+		return success
+	}
+	remove(key: string | string[]): void {
+		if (!key || this.isEmpty) return
+		if (isString(key)) {
+			return this.#storage.removeItem(this.#prefixKey(key))
+		}
+		let index = -1
+		const length = key.length
+		while (++index < length) {
+			this.#storage.removeItem(this.#prefixKey(key[index]))
+		}
+	}
+	clear(excludes?: string | string[]): void {
+		if (this.length <= 0) return
+		if (excludes === undefined || excludes.length <= 0) {
+			this.#storage.clear()
+			return
+		}
+		const keys: string[] = isString(excludes) ? [excludes] : excludes
+		this.remove(this.keys().filter(key => !keys.includes(key)))
+	}
+	get length(): number {
+		return this.#storage.length
+	}
+	get isEmpty(): boolean {
+		return this.length <= 0
+	}
+	get size(): number {
+		if (this.isEmpty) return 0
+		const data = this.values().toString()
+
+		if (!(data && data.length)) return 0
+		let total = 0
+		let index = -1
+		const length = data.length
+		if (length <= 0) return total
+		while (++index < length) {
+			total += data.charCodeAt(index) <= 0xffff ? 2 : 4
+		}
+		return total
+	}
+	key(index: number): string | null {
+		if (this.isEmpty) return null
+		const key = this.#storage.key(index)
+		return key ? this.#unPrefixKey(key) : key
+	}
+	keys(): string[] {
+		return Object.keys(this.#storage).map(key => this.#unPrefixKey(key))
+	}
+	values(): any[] {
+		if (this.length <= 0) return []
+		return this.keys().map(key => this.get(key))
+	}
+	#prefixKey(key: string): string {
+		return `${this.#config.prefix || ""}${key}`
+	}
+	#unPrefixKey(key: string): string {
+		return this.#config.prefix ? key.slice(this.#config.prefix.length) : key
+	}
 }
 
 export class LocalStorage extends StorageCore {
-  constructor(config?: StorageConfig) {
-    super(localStorage, config)
-  }
+	constructor(config?: StorageConfig) {
+		super(localStorage, config)
+	}
 }
 
 export class SessionStorage extends StorageCore {
-  constructor(config: StorageConfig) {
-    super(sessionStorage, config)
-  }
+	constructor(config: StorageConfig) {
+		super(sessionStorage, config)
+	}
 }
+
